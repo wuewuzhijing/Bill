c1914fea274880f3b7a151b8ca4eb7e8282f85a9
ly <15820480843@163.com>
2017-10-27 20:10:11 +0800

    完成添加发票的页面以及优化接口封装

--- pages/addTitle/addTitle.js
+++ pages/addTitle/addTitle.js
@@ -1,12 +1,24 @@
 // pages/addTitle/addTitle.js
-var WxSearch = require('../../wxSearch/wxSearch.js')
+var WxSearch = require('../../wxSearch/wxSearch.js');
+var util = require("../../utils/util.js");
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-   
+    radioCheck:0,
+    type:0,
+
+    userId:"",
+    headType: 1,
+    headName: "666",
+    invoiceType: 1,
+    taxNo: "",
+    address: "",
+    telephone: "",
+    bankName: "",
+    bankAccount: "",   
   },
 
   /**
@@ -14,9 +26,13 @@ Page({
    */
   onLoad: function (options) {
     var that = this
+
     //初始化的时候渲染wxSearchdata
     WxSearch.init(that, 43, ['weappdev', '小程序', 'wxParse', 'wxSearch', 'wxNotification']);
-    WxSearch.initMindKeys(['weappdev.com', '微信小程序开发', '微信开发', '微信小程序']);
+    WxSearch.initMindKeys(['weappdev.com', '微信小程序开发', '微信开发', '微信小程序', 'weappdev', '小程序', 'wxParse', 'wxSearch', 'wxNotification']);
+    this.setData({
+      //  怎么设置标题的文字
+    })
   },
 
   /**
@@ -61,13 +77,13 @@ Page({
   
   },
 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+  radioCheckedChange: function (e) {
+    console.log(e.detail.value);
+    this.setData({
+      radioCheck: e.detail.value,
+      type: e.detail.value
+    })
   },
-
 
   wxSearchInput: function (e) {
     console.log(e);
@@ -87,6 +103,69 @@ Page({
     wx.navigateTo({
       url: '../index/index'
     })
+  },
+  
+  clearMessage:function(){
+    var that = this;
+    console.log(123)
+    util.clearError(that);
+  },
+
+  formSubmit(e){
+    var data = e.detail.value;
+    var that = this;
+    data.headType="1";
+    data.userId = "567";
+    data.invoiceType = "1";
+
+    console.log(data);
+
+    // if (data.surplus == '0') {
+    //   var arr = new Array('', '剩余空位', '乘车人数');
+    //   util.isError('请选择' + arr[data.type], that);
+    //   return false;
+    // }
+
+
+    if (that.data.type == 0){
+      if (data.headName == '') {
+        util.isError('请输抬头', that);
+        return false;
+      }
+      if (data.taxNo == '') {
+        util.isError('请输入税号', that);
+        return false;
+      }
+      
+    } else if (that.data.type == 1){
+
+    } else if (that.data.type == 2){
+
+    }
+
+    console.log("开始提交");
+    util.getQuery('invoice/addInvoiceHead' , data , "加载中" , function success(res){
+      if(res == false){
+        console.log(res.data.returnmessage);
+      }else{
+        console.log(res);
+        wx.showToast({
+          title: '提交成功',
+          icon: 'success',
+          duration: 2000
+        })
+      }
+      
+    },function fail(res){
+      // util.showToastErr(res.data.returnmessage);
+      console.log(res);
+      if (res.data.returnMessage != ""){
+        util.isError("" + res.data.returnMessage, that);
+      }
+     
+    })
   }
+
+  
  
 })
--- utils/util.js
+++ utils/util.js
@@ -14,6 +14,66 @@ const formatNumber = n => {
   return n[1] ? n : '0' + n
 }
 
+// var rootDocment = 'https://dev.bookingyun.com/CenterMaster/';
+var rootDocment = 'https://dev.bookingyun.com/CenterMaster/';
+
+
+function getQuery(url, parms, message, success, fail) {
+
+  if (message != "") {
+    wx.showLoading({
+      title: message,
+    })
+  }
+
+  wx.request({
+    url: rootDocment + url,
+    // sName: 'user/sendVerifyCode',  // 这两个参数的作用
+    // text: '发送验证码',
+    method: 'GET',
+    data: parms,
+    isLoading: true,
+    header: {
+      'content-type': 'application/x-www-form-urlencoded'
+    },
+    success: function (res) {
+      if (message != "") {
+        wx.hideLoading()
+      }
+
+      if (res.data.returnCode == '1') {
+        success(res);
+      }else{
+        fail(res);
+      }
+    },
+    fail: function () {
+      if (message != "") {
+        wx.hideLoading();
+      }
+    }
+  })
+}
+
+
+// 提示错误信息
+function isError(msg, that) {
+  that.setData({
+    showTopTips: true,
+    errorMsg: msg
+  })
+}
+// 清空错误信息
+function clearError(that) {
+  that.setData({
+    showTopTips: false,
+    errorMsg: ""
+  })
+}
+
 module.exports = {
-  formatTime: formatTime
+  formatTime: formatTime,
+  getQuery: getQuery,
+  isError: isError,
+  clearError: clearError
 }
